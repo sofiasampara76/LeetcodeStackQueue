a4b0110d75d027460228d668ff9014d4c213decd
Sofia Sampara <sampara.pn@ucu.edu.ua>
2024-04-15 20:58:57 +0300

    Improved the freq stack task

--- Maximum Frequency Stack/solution.py
+++ Maximum Frequency Stack/solution.py
@@ -4,21 +4,25 @@ class FreqStack:
 
     def __init__(self):
         self.stack = deque()
+        self.dict_of_values = {}
 
     def push(self, val: int) -> None:
-        self.stack.appendleft(val)
+        self.stack.append(val)
+        if val in self.dict_of_values:
+            self.dict_of_values[val] += 1
+        else:
+            self.dict_of_values[val] = 1
 
     def pop(self) -> int:
-        if self.stack:
-            set_of_elements = set()
-            counter = 0
-            val = None
-            for el in self.stack:
-                if el not in set_of_elements:
-                    number = self.stack.count(el)
-                    set_of_elements.add(el)
-                    if number > counter:
-                        counter, val = number, el
-            self.stack.remove(val)
-            return val
-        return None
+        max_freq = max(self.dict_of_values.values())
+        popped_values = []
+        popped = -1
+        while self.stack:
+            popped = self.stack.pop()
+            if max_freq != self.dict_of_values[popped]:
+                popped_values.append(popped)
+            else:
+                self.dict_of_values[popped] -=1
+                break
+        self.stack.extend(list(reversed(popped_values)))
+        return popped
